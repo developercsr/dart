611c2d9aeb09cd604a9bb4620f38d063884774da
developercsr <sumanthroyal111@gmail.com>
2024-01-27 14:48:36 +0530

    data types in dart

---
+++ dart2.dart
@@ -0,0 +1,48 @@
+//Data Types
+//=>INTEHERS
+var a=1;
+double b=1.1;
+void main(){
+  print("${a},${b}");
+  strings();
+}
+//=>SRINGS
+var c="Hari om";
+strings(){
+  bool d=true;
+  print(c);
+  print(d);
+  lists();
+}
+//=>lists and Maps
+lists(){
+  var list1=[1,2,3,4,5,6,7,8,9];
+  var maps1={1:2,2:3,3:4,4:5};
+  print(list1);
+  print(maps1);
+  main1();
+}
+//=>runes => Symbols in dart
+void main1(){  
+    var heart_symbol = '\u2665';  
+    var laugh_symbol = '\u{1f600}';  
+    print(heart_symbol);  
+    print(laugh_symbol);
+    int inn;
+    var in1;
+    in1="123";
+    print(in1);
+    in1=123;
+    print(in1);
+    inn=10;
+    print(inn);
+    mm();
+}
+var de;
+mm(){
+  print(de);//The value will be NULL
+}
+
+//final key word
+
+final ab=12;

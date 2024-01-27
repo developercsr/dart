df194ed2dc2de48e3a148f480b0dbb9f4acc667f
developercsr <sumanthroyal111@gmail.com>
2024-01-27 17:37:51 +0530

    Numbers Methods and Properties

--- dart5.dart
+++ dart5.dart
@@ -9,4 +9,42 @@ void main()
   double b = 8989.7768787675764;
   print(a);
   print(b);
+  properties();
+}
+//Number Properties in DART
+properties()
+{
+  int a=90;
+  double b=23;
+  double c=667576576565;
+  print("PROPERTIES OF THE NUMBERS : ");
+  print(a);
+  print(a.hashCode);
+  print(a.isEven);
+  print(a.isOdd);
+  print(a.isFinite);
+  print("${b}\n${c}");
+  print(b.isFinite);
+  print(c.isInfinite);
+  print(b.isNaN);
+  print(c.isNegative);
+  methods();  
+}
+methods()
+{
+    print("Methods for NUMBERS : ");
+    int a=-12;
+    int b=65;
+    double c=45454.4345;
+    print(a.abs());
+    print(c.ceil());
+    print(c.floor());
+    print(b.compareTo(a));//1 if b>a and 0 if b==a and -1 if b<a
+    print(c.sign);//1.0 for + ;; -1.0 for - ;; 0.0 for == ;; Nan for Nan value 
+    print(c.remainder(3));
+    print(c.round());
+    print(a.toDouble());
+    print(c.toInt());
+    print(c.toString());
+    print(c.truncate());        
 }

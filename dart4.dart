995336eb001afc03786519547393cb2f061c06cb
developercsr <sumanthroyal111@gmail.com>
2024-01-27 17:05:16 +0530

    Constants

---
+++ dart4.dart
@@ -0,0 +1,11 @@
+///dart constant a immutable object
+///we can make a consrant by using 2 key word they are
+///final 
+///const
+void main()
+{
+  final int  a=10;
+  const String b ="hari om";
+  print(a);
+  print(b);
+}

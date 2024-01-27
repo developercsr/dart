2d37e7598b3e33e33e8c86ada83f44d960a915d7
developercsr <sumanthroyal111@gmail.com>
2024-01-27 17:05:39 +0530

    Numbers

---
+++ dart5.dart
@@ -0,0 +1,12 @@
+///Numbers in Dart
+///2 Types
+///int ==> stores values between -263 to 263
+///double remaining all numbers can be stored with the helop of this double keyword
+///including numbers
+void main()
+{
+  int a=44;
+  double b = 8989.7768787675764;
+  print(a);
+  print(b);
+}

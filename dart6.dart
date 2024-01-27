2c6c51655e8e89c6cf35a5110b9aad61437f57b0
developercsr <sumanthroyal111@gmail.com>
2024-01-27 18:03:20 +0530

    Strings

---
+++ dart6.dart
@@ -0,0 +1,16 @@
+///Dart Strings
+void main()
+{
+  String a="Hari Om,I am sumanth. I am Working in DhiyoTek";
+  String b="""I am 
+  Sumnath
+  ,Working In Dhiyotek
+  !""";
+  print(a);
+  print(b);
+  print("String 3 is ${a+b}");
+  print("Properties Of Strings : ");
+  print(a.codeUnits);
+  print(a.isEmpty);
+  print(b.length);
+}

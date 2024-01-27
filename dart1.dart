a3fb40ce0b27055293a6ddfa689ca963bb3d6846
developercsr <sumanthroyal111@gmail.com>
2024-01-27 13:30:30 +0530

    VARIBLES,COMMENTS

---
+++ dart1.dart
@@ -0,0 +1,24 @@
+//first program ==>SINGLE LINE PROGRAMMING
+main(){
+  print("Hari om");
+  fun();
+}
+
+/*MULTILINE PROGRAMMING
+Identifiers are naem of the variables 
+=>starts should be alphabet,$,_
+=> successive underscore is not allowed
+ */
+
+var a=23;
+var b="sumanth";
+void fun(){
+print("my name is ${a},My age is ${b}");
+}
+///
+///These are all DOCUMENATION COMMENTS
+///ALL THREE ARE GENERATED ON ENTER COMMAND
+///
+///
+///
+///

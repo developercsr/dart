cdea1706aa8b8eb6a92c8771ede621b26ecf7b22
developercsr <sumanthroyal111@gmail.com>
2024-01-29 12:52:31 +0530

    polymorphism override

---
+++ dart99.dart
@@ -0,0 +1,22 @@
+//polymorphism
+class A{
+  Aa(){
+    print("Hari om");
+  }
+}
+class B extends A{
+ Bb()
+ {
+  print("I am Sumanth");
+ }
+ @override
+ Aa()
+ {
+  print("I m Working in DhiyoTech!");
+ }
+}
+void main()
+{
+  B aa=B();
+  aa.Aa();
+}
